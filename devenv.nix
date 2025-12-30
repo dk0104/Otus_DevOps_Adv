8da7b0d9f889d79c90d1bec32b6480e49d9fb317
denk <kexsel@yandex.ru>
2025-12-30 21:54:58 +0400

    gitops

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,8 @@
 {
   pkgs,
-  lib,
-  config,
-  inputs,
   ...
-}: {
+}:
+{
   # https://devenv.sh/basics/
   env.GREET = "Wellcome to DevOps Advanced setup";
 
@@ -15,15 +13,6 @@
     terraform
     yandex-cloud
   ];
-
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
   scripts.hello.exec = ''
@@ -39,21 +28,4 @@
   enterShell = ''
     hello         # Run scripts directly
   '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
